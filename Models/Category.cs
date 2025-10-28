f1622e045d327bac27fca5a5151389ca244ab1c9
Stefcho18 <stefchoplamenovk@gmail.com>
2025-10-28 20:00:45 +0200

    MakingModelsStructure

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChineseFood.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public List<Product> FoodItems { get; set; }
+    }
+}
